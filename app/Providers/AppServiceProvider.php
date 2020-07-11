6f4dfe9fbb8be6ccf65d1eb8bf977d11dc88a282
Stefan Avlijas <6214951-avlijastefan@users.noreply.gitlab.com>
2020-07-11 16:13:53 +0200

    update service provider 2

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -28,13 +28,12 @@ class AppServiceProvider extends ServiceProvider
 
         Validator::extend('alphanum_spaces', function ($attribute, $value) {
             return preg_match('/^[a-z0-9 .\-]+$/i', $value); 
-    });
+        });
         Validator::extend('uniqueFirstAndLastName', function ($attribute, $value, $parameters, $validator) {
             $count = DB::table('authors')->where('first_name', $value)
                                          ->where('last_name', $parameters[0])
                                          ->count();
-            
-                return $count === 0;
-            });
+            return $count === 0;
+        });
     }
 }
